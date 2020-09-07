c5fa69e8e518fe7e7527f3f0f46abe221303e56e
Satvik Ramaprasad <satviksr@gmail.com>
2020-09-07 09:31:48 +0530

    Conflict fix

---
+++ public/src/hotkey_binder/defaultKeys.js
@@ -0,0 +1,29 @@
+/**Add more elements here, along with a valid value for key
+* Elements keys must have the same name as their ID
+**/
+
+export const defaultKeys = {
+  "New Circuit": "Shift + N",
+  "Save Online": "Ctrl + S",
+  "Save Offline": "Ctrl + Alt + S",
+  "Download as Image": "Ctrl + D",
+  "Open Offline": "Ctrl + O",
+  "Insert Sub-circuit": "Shift + C",
+  "Combinational Analysis": "Shift + A",
+  // "Start Plot": "Ctrl + P",
+  "Direction Up": "Up",
+  "Direction Down": "Down",
+  "Direction Left": "Left",
+  "Direction Right": "Right",
+  "Insert Label": "Ctrl + L",
+  "Label Direction Up": "Alt + Up",
+  "Label Direction Down": "Alt + Down",
+  "Label Direction Left": "Alt + Left",
+  "Label Direction Right": "Alt + Right",
+  "Move Element Up": "W",
+  "Move Element Down": "S",
+  "Move Element Left": "A",
+  "Move Element Right": "D",
+  "Hotkey Preference": "F8",
+};
+
